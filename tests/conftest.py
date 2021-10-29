4d720e6a3a9918281d1efa9a95907bcd452fd7e2
Melvin Wang <melvin.mc.wang@gmail.com>
2021-10-29 18:39:47 -0400

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,16 @@ import pytest
 
 from pathlib import Path
 
+
 @pytest.fixture()
 def test_data_path(monkeypatch) -> Path:
-    return (Path(__file__)/'..'/'data').resolve(strict=True)
+    return (Path(__file__) / '..' / 'data').resolve(strict=True)
 
 @pytest.fixture(autouse=True)
-def default_test_env(monkeypatch, test_data_path):
-    monkeypatch.setenv(name='SLIC3R_PP_OUTPUT_NAME', value=str(test_data_path/'generated_test_output.g'))
+def test_env(monkeypatch, test_data_path, request):
+    test_name = request.param
+    monkeypatch.setenv(name='SLIC3R_PP_OUTPUT_NAME', value=str(test_data_path / f'{test_name}.gcode'))
     monkeypatch.setenv(name='SLIC3R_TRAVEL_SPEED', value='130')
     monkeypatch.setenv(name='SLIC3R_TRAVEL_SPEED_Z', value='7')
 
+    return {'test_name': test_name}
